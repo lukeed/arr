cfed53d684af0831a667462a0c1ba647ee8df07f
Luke Edwards <luke.edwards05@gmail.com>
2017-07-06 10:51:06 -0700

    import `map-arr` as `@arr/map`

---
+++ packages/map/module.js
@@ -0,0 +1,13 @@
+export default function (arr, fn) {
+  if (arr == null) {
+    return [];
+  }
+
+  var i=0, len=arr.length, out=new Array(len);
+
+  for (; i < len; i++) {
+    out[i] = fn(arr[i], i);
+  }
+
+  return out;
+}
