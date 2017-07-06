05134e8da690e68bd5f1ecbc0bc6b91a6f616aba
Luke Edwards <luke.edwards05@gmail.com>
2017-07-06 12:30:53 -0700

    add `arr/reduce` with benchmarks

---
+++ packages/reduce/module.js
@@ -0,0 +1,18 @@
+export default function (arr, fn, val) {
+  if (arr == null) {
+    return [];
+  }
+
+  var i=0, len=arr.length, out=val;
+
+  if (out === void 0) {
+  	out = arr[0];
+  	i = 1;
+  }
+
+  for (; i < len; i++) {
+  	out = fn(out, arr[i], i, arr);
+  }
+
+  return out;
+}
