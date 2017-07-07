bcbb066ecfcbb9af6faddccced5ba061bfd26559
Luke Edwards <luke.edwards05@gmail.com>
2017-07-07 16:40:42 -0700

    consolidate `arr/flatten` code

--- packages/flatten/module.js
+++ packages/flatten/module.js
@@ -1,6 +1,5 @@
 function flat(arr, res) {
-	var i = 0, cur;
-	var len = arr.length;
+	var i=0, cur, len=arr.length;
 	for (; i < len; i++) {
 		cur = arr[i];
 		Array.isArray(cur) ? flat(cur, res) : res.push(cur);
