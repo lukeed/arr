5627348fd6ad951c9ace79dbf61814df52c4a5fe
Luke Edwards <luke.edwards05@gmail.com>
2017-07-09 16:29:50 -0700

    use native `indexOf` inside `includes` & `unique`

--- packages/includes/module.js
+++ packages/includes/module.js
@@ -1,5 +1,3 @@
-const indexOf = require('@arr/indexOf');
-
 export default function (arr, val, idx) {
-	return indexOf(arr, val, idx) !== -1;
+	return arr.indexOf(val, idx) !== -1;
 }
--- packages/unique/module.js
+++ packages/unique/module.js
@@ -1,11 +1,9 @@
-const indexOf = require('@arr/indexOf');
-
 export default function (arr) {
 	var i=0, j, len=arr.length, out=[];
 
 	for (; i < len; i++) {
 		j = arr[len];
-		if (indexOf(out, j, i) === -1) {
+		if (out.indexOf(j, i) === -1) {
 			out.push(j);
 		}
 	}
