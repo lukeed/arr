7791e5b80f2ebdc089f2d18fe44a301cb7902af7
Luke Edwards <luke.edwards05@gmail.com>
2017-07-11 22:21:19 -0700

    fix `arr/filter` callback

--- packages/filter/module.js
+++ packages/filter/module.js
@@ -2,7 +2,7 @@ export default function (arr, cb) {
 	var len = arr.length;
 
 	while (len--) {
-		cb(arr[len]) || arr.splice(len, 1);
+		cb(arr[len], len, arr) || arr.splice(len, 1);
 	}
 
 	return arr;
